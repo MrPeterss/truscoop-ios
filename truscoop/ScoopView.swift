8865e3b75443c057f300400eaf6e65ec6ab7d3aa
peter <pjb294@cornell.edu>
2023-11-28 17:29:38 -0500

    Some fixes

--- truscoop/ScoopView.swift
+++ truscoop/ScoopView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct ScoopView: View {
     
     @State var scoop: Scoop
+    @State var selectedRating: String = "none"
     
     var body: some View {
         // Header
@@ -22,18 +23,34 @@ struct ScoopView: View {
         }
     }
     
+    private func userInButton(color: String, value: String) -> some View {
+        Button {
+            print("selected rating set to \(value)")
+            selectedRating = value
+        } label: {
+            RoundedRectangle(cornerRadius: 5)
+                .fill(Color(hex: color)!)
+                .stroke(selectedRating == value ? .black: .clear, lineWidth: 3)
+                .shadow(color: .black.opacity(0.5), radius: 1, x: 0, y: 1)
+
+        }
+    }
+    
     private var headerImage: some View {
         VStack {
-            GeometryReader {geometry in
-                AsyncImage(url: URL(string: scoop.top_img)) { image in
-                    image.resizable()
-                        .aspectRatio(contentMode: .fill)
-                        .edgesIgnoringSafeArea(.top)
-                        .colorMultiply(.gray)
-                } placeholder: {
-                    Rectangle()
+            ZStack {
+                GeometryReader {geometry in
+                    AsyncImage(url: URL(string: scoop.top_img)) { image in
+                        image.resizable()
+                            .aspectRatio(contentMode: .fill)
+                            .edgesIgnoringSafeArea(.top)
+                    } placeholder: {
+                        Rectangle()
+                    }
+                    .frame(width: geometry.size.width, height: geometry.size.height * 0.5)
                 }
-                .frame(width: geometry.size.width, height: geometry.size.height * 0.5)
+                Rectangle()
+                    .fill(.black.opacity(0.75))
             }
             
             Spacer()
@@ -89,9 +106,9 @@ struct ScoopView: View {
                 .frame(width: 60, height: 8)
             
             Spacer()
-        }.offset(x: 0)
+        }
     }
-
+    
     private var article: some View {
         GeometryReader { geometry in
             ZStack {
@@ -99,20 +116,34 @@ struct ScoopView: View {
                     .fill(.white)
                     .cornerRadius(30)
                     .frame(width: geometry.size.width, height: 1000, alignment: .center)
-                    
-                VStack {
-                    Text(scoop.summary).padding(30)
-                        .frame(width: geometry.size.width * 0.8, height: geometry.size.height * 0.3, alignment: .center)
+                VStack(alignment: .leading) {
+                    Text(scoop.summary)
+                        .font(.custom("Futura", size: 15))
+                        .frame(height: 100)
+                        .lineLimit(4)
                     
                     // Your Rating
-                    HStack(spacing: 3) {
-                        Text("Your")
-                            .font(.system(size: 18, weight: .bold))
-                            .bold()
-                            .foregroundColor(.black)
-                        Text("Rating").font(.system(size: 18, weight: .regular))
-                            .foregroundColor(.black)
+                    VStack {
+                        HStack(spacing: 3) {
+                            Text("Your")
+                                .font(.custom("Futura Bold", size: 18))
+                                .foregroundColor(.black)
+                            Text("Rating")
+                                .font(.custom("Futura", size: 18))
+                                .foregroundColor(.black)
+                            Spacer()
+                        }
+                        HStack(spacing: 10) {
+                            userInButton(color: "0F7FBC", value: "very liberal")
+                            userInButton(color: "5AB0EE", value: "liberal")
+                            userInButton(color: "909090", value: "neutral")
+                            userInButton(color: "E86868", value: "conservative")
+                            userInButton(color: "BC5050", value: "very conservative")
+                        }
+                        .frame(height: 30)
+                        .padding(EdgeInsets(top: 0, leading: 5, bottom: 0, trailing: 5))
                     }
+                    .padding(EdgeInsets(top: 0, leading: 0, bottom: 45, trailing: 0))
                     
                     // Button to webview
                     NavigationLink {
@@ -123,18 +154,21 @@ struct ScoopView: View {
                             Rectangle()
                                 .fill(Color.init(hex: "E1E1E1")!)
                                 .cornerRadius(20)
-                                .frame(width: geometry.size.width * 0.8, height: 44)
+                                .frame(height: 44)
+                                .shadow(color: .black.opacity(0.25), radius: 2, x: 0, y: 2)
                             
                             Text("Read this Article")
                                 .font(.system(size: 18, weight: .bold))
                                 .foregroundColor(.black)
                         }
                     }
-                    
                     Spacer()
                 }
-                
-            }.safeAreaPadding(.bottom)
+                .padding(EdgeInsets(top: 15, leading: 25, bottom: 0, trailing: 25))
+            }
+            .safeAreaPadding(.bottom)
+            
+            .padding(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
             .offset(y: geometry.size.height * 0.35)
         }
     }
