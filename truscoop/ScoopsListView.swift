a9e10da02a73c36f3a6712ab1285136a99925bbc
peter <pjb294@cornell.edu>
2023-11-27 21:23:51 -0500

    UI Updates

--- truscoop/ScoopsListView.swift
+++ truscoop/ScoopsListView.swift
@@ -17,83 +17,87 @@ struct ScoopsListView: View {
                     ScoopView(scoop: scoop)
                 } label: {
                     EmptyView()
-                }.opacity(0.0)
+                }
+                .opacity(0.0)
+
             }
-            .listRowBackground(
-                RoundedRectangle(cornerRadius: 8)
-                    .background(.clear)
-                    .foregroundColor(Color(hex: "f3f3f3"))
-            )
-            .padding(EdgeInsets(top: 10, leading: 0, bottom: 10, trailing: 0))
             .listRowSeparator(.hidden)
             .listStyle(.plain)
             Spacer().zIndex(-1)
         }
         .scrollIndicators(.hidden)
         .listStyle(.plain)
-        .padding()
+        .padding(EdgeInsets(top: 10, leading: 10, bottom: 10, trailing: 10))
+        .buttonStyle(PlainButtonStyle())
         
     }
     
     private func scoopRow(_ article: Scoop) -> some View {
-        HStack (alignment: .top) {
-            VStack (alignment: .leading) {
-                Image(article.favicon)
-                    .resizable()
-                    .frame(width: 32, height: 32)
-                    .cornerRadius(8)
+        ZStack {
+            RoundedRectangle(cornerRadius: 8)
+                .background(.clear)
+                .foregroundColor(Color(hex: "f3f3f3"))
+                .shadow(color: .black.opacity(0.5), radius: 3, x: 0, y: 4)
+            
+            HStack (alignment: .top) {
+                VStack (alignment: .leading) {
+                    Image(article.favicon)
+                        .resizable()
+                        .frame(width: 32, height: 32)
+                        .cornerRadius(8)
+                    
+                    Text(article.name)
+                        .font(.custom("Futura Bold Condensed", size: 26))
+                        .lineLimit(/*@START_MENU_TOKEN@*/2/*@END_MENU_TOKEN@*/)
+                        .fixedSize(horizontal: false, vertical: true)
+                    Spacer()
+                    Text(article.summary)
+                        .font(.system(size: 14))
+                        .lineLimit(4)
+                        .fixedSize(horizontal: false, vertical: true)
+                }
                 
-                Text(article.name)
-                    .font(.custom("Futura Bold Condensed", size: 26))
-                    .lineLimit(/*@START_MENU_TOKEN@*/2/*@END_MENU_TOKEN@*/)
-                    .fixedSize(horizontal: false, vertical: true)
                 Spacer()
-                Text(article.summary)
-                    .font(.system(size: 14))
-                    .lineLimit(4)
-                    .fixedSize(horizontal: false, vertical: true)
-            }
-            
-            Spacer()
-            
-            VStack (alignment: .trailing){
-                VStack (alignment: .trailing, spacing: 2) {
-                    Rectangle()
-                        .foregroundColor(.clear)
-                        .frame(width: 49, height: 8)
-                        .background(
-                            Group {
-                                switch article.ai_rating {
-                                case "Left":
-                                    Color.blue
-                                case "Right":
-                                    Color.red
-                                default:
-                                    Color.gray
+                
+                VStack (alignment: .trailing){
+                    VStack (alignment: .trailing, spacing: 2) {
+                        Rectangle()
+                            .foregroundColor(.clear)
+                            .frame(width: 49, height: 8)
+                            .background(
+                                Group {
+                                    switch article.ai_rating {
+                                    case "Left":
+                                        Color.blue
+                                    case "Right":
+                                        Color.red
+                                    default:
+                                        Color.gray
+                                    }
                                 }
-                            }
-                        )
-                    Rectangle()
-                        .foregroundColor(.clear)
-                        .frame(width: 26, height: 3)
-                        .background(.black)
-                }
-                HStack(spacing: 0) {
-                    Text(String(article.user_rating))
-                        .font(.custom("Futura Bold", size: 13))
-                    Text("/5")
-                        .font(.custom("Futura Bold", size: 13))
-                }
-                Spacer()
-                HStack (spacing: 5) {
-                    Text("More")
-                        .font(.system(size: 14, weight: .bold))
-                    Image(systemName: "arrow.forward")
-                    
-                    
+                            )
+                        Rectangle()
+                            .foregroundColor(.clear)
+                            .frame(width: 26, height: 3)
+                            .background(.black)
+                    }
+                    HStack(spacing: 0) {
+                        Text(String(article.user_rating))
+                            .font(.custom("Futura Bold", size: 13))
+                        Text("/5")
+                            .font(.custom("Futura Bold", size: 13))
+                    }
+                    Spacer()
+                    HStack (spacing: 5) {
+                        Text("More")
+                            .font(.system(size: 14, weight: .bold))
+                        Image(systemName: "arrow.forward")
+                        
+                        
+                    }
                 }
             }
-            .frame(height: 136, alignment: .top)
+            .padding()
         }
     }
 }
